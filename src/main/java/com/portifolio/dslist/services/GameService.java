bc28d2f170d47ec0eb8c5b6a7087f38583021b94
Henrique Bisneto <rick240799@gmail.com>
2023-05-18 12:07:35 -0300

    Domain Model

--- src/main/java/com/portifolio/dslist/services/GameService.java
+++ src/main/java/com/portifolio/dslist/services/GameService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.portifolio.dslist.dto.GameMinDTO;
+import com.portifolio.dslist.dto.GameMinDTO;  
 import com.portifolio.dslist.entities.Game;
 import com.portifolio.dslist.repositories.GameRepository;
 
